493261ef35ab4e1497f840329c61515039240fe9
Ilya Tsyplenkov <ilya@mindsdb.com>
2022-07-29 17:06:04 +0300

    Bi-directional streaming

--- currency/data/rates.go
+++ currency/data/rates.go
@@ -3,8 +3,10 @@ package data
 import (
 	"encoding/xml"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/hashicorp/go-hclog"
 )
@@ -22,6 +24,36 @@ func NewExchangeRates(l hclog.Logger) (*ExchangeRates, error) {
 	return er, err
 }
 
+// MonitorRates checks the rates in the ECB API every interval and sends a message to the
+// returned channel when there are changes
+//
+// Note: the ECB API only returns data once a day, this function only simulates the changes
+// in rates for demonstration purposes
+func (e *ExchangeRates) MonitorRates(interval time.Duration) <-chan struct{} {
+	ret := make(chan struct{})
+	go func() {
+		ticker := time.NewTicker(interval)
+		for {
+			select {
+			case <-ticker.C:
+				for k, v := range e.rates {
+					change := (rand.Float64() / 10)
+					direction := rand.Intn(1)
+					if direction == 0 {
+						change = 1 - change
+					} else {
+						change = 1 + change
+					}
+					e.rates[k] = v * change
+				}
+				ret <- struct{}{}
+			}
+		}
+
+	}()
+	return ret
+
+}
 func (e *ExchangeRates) GetRate(base, dest string) (float64, error) {
 	br, ok := e.rates[base]
 	if !ok {
--- currency/server/currency.go
+++ currency/server/currency.go
@@ -12,16 +12,36 @@ import (
 )
 
 type Currency struct {
-	rates *data.ExchangeRates
-	log   hclog.Logger
-	protos.UnimplementedCurrencyServer
+	rates         *data.ExchangeRates
+	log           hclog.Logger
 	subscriptions map[protos.Currency_SubscribeRatesServer][]*protos.RateRequest
+	protos.UnimplementedCurrencyServer
 }
 
 func NewCurrency(r *data.ExchangeRates, l hclog.Logger) *Currency {
-	return &Currency{rates: r, log: l}
+	currency := &Currency{rates: r, log: l, subscriptions: make(map[protos.Currency_SubscribeRatesServer][]*protos.RateRequest)}
+	go currency.handleUpdates()
+	return currency
 }
 
+func (c *Currency) handleUpdates() {
+	ru := c.rates.MonitorRates(5 * time.Second)
+	for range ru {
+		for k, v := range c.subscriptions {
+			for _, rr := range v {
+				rate, err := c.rates.GetRate(rr.Base.String(), rr.Destination.String())
+				if err != nil {
+					c.log.Error("unable to get rates", "Base", rr.Base.String(), "Destination", rr.Destination.String())
+				}
+				if err := k.Send(&protos.RateResponse{Base: rr.Base, Destination: rr.Destination, Rate: rate}); err != nil {
+					c.log.Error("unable to send updated rate", "Base", rr.Base.String(), "Destination", rr.Destination.String())
+				}
+
+			}
+		}
+
+	}
+}
 func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos.RateResponse, error) {
 	c.log.Info("Handle GetRate", "base", rr.GetBase(), "destination", rr.GetDestination())
 	rate, err := c.rates.GetRate(rr.GetBase().String(), rr.GetDestination().String())
@@ -33,26 +53,25 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 }
 
 func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) error {
-	go func() {
-		for {
-			rr, err := src.Recv()
-			if err == io.EOF {
-				c.log.Info("client has closed the connection.")
-				break
-			}
-			if err != nil {
-				c.log.Error("unable to read from client", "error", err)
-				break
-			}
-			c.log.Info("handle client request", "request", rr)
-		}
-	}()
-
 	for {
-		err := src.Send(&protos.RateResponse{Rate: 12.1})
+		rr, err := src.Recv()
+		if err == io.EOF {
+			c.log.Info("client has closed the connection.")
+			break
+		}
 		if err != nil {
+			c.log.Error("unable to read from client", "error", err)
 			return err
 		}
-		time.Sleep(5 * time.Second)
+		c.log.Info("handle client request", "Base", rr.Base.String(), "Destionation", rr.Destination.String())
+
+		rrs, ok := c.subscriptions[src]
+		if !ok {
+			rrs = make([]*protos.RateRequest, 0)
+		}
+		rrs = append(rrs, rr)
+		c.subscriptions[src] = rrs
 	}
+
+	return nil
 }
